9af9862733ee8b83c8bc19f7120600bfd447b3c6
arturtarcisio <arturtarcisio1@gmail.com>
2024-05-14 16:36:32 -0300

    HU.03 - Criando car repository

---
+++ backend/src/main/java/io/github/arturtcs/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package io.github.arturtcs.repository;
+
+import io.github.arturtcs.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
