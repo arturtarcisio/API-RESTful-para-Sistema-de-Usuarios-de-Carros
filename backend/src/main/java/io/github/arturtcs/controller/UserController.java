1539edd55b28a0c9d3eb7c20b93d5aec07fb9684
arturtarcisio <arturtarcisio1@gmail.com>
2024-05-14 23:19:16 -0300

    HU.11 - Trocando os retornos dos endpoint

--- backend/src/main/java/io/github/arturtcs/controller/UserController.java
+++ backend/src/main/java/io/github/arturtcs/controller/UserController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,26 +21,21 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> listarTodosOsUsuarios() {
-        return ResponseEntity.ok(userService.showUsers());
+    public List<User> listarTodosOsUsuarios() {
+        return userService.showUsers();
     }
 
     @PostMapping
-    public ResponseEntity<User> cadastrarUsuario (@RequestBody User user) {
-        user = userService.registerUser(user);
-        URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(user.getId())
-                .toUri();
-        return ResponseEntity.created(uri).body(user);
+    @ResponseStatus(HttpStatus.CREATED)
+    public User cadastrarUsuario (@RequestBody User user) {
+        return userService.registerUser(user);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById (@PathVariable Long id) {
-        return userService.findUserById(id)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public User findById (@PathVariable Long id) {
+        return userService
+                .findUserById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
     }
 
     @DeleteMapping("/{id}")
