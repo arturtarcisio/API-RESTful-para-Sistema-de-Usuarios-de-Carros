67f9b51013bec796745f8bffefd1a56898220fcb
arturtarcisio <arturtarcisio1@gmail.com>
2024-05-19 18:03:21 -0300

    HU55 - Validação de formularios para login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginRequest } from '../../model/loginRequest';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  
+  loginRequest: LoginRequest = {
+    login: '',
+    password: ''
+  }
+
+  login = new FormControl(null, Validators.required)
+  password = new FormControl(null, Validators.minLength(3))
+  
+  constructor(){}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  validaCampos(): boolean {
+    if(this.login.valid && this.password.valid){
+      return true
+    } else {
+      return false
+    }
+
+  }
 
 }
