3d3ec1a10752e273e5a2e01b26c68de14a3aee3f
arturtarcisio <arturtarcisio1@gmail.com>
2024-05-20 00:05:10 -0300

    HU67 - Adicionando rotas no interceptor que não irão precisa de token de autenticacao

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,21 +1,33 @@
-import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(){}
+  private excludedRoutes: string[] = [
+    `${API_CONFIG.baseUrl}/users/**`,
+    `${API_CONFIG.baseUrl}/signin`
+  ];
+
+  constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token = localStorage.getItem('token')
-    
-    if(token){
-      const cloneReq = 
-        req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`)})
-        return next.handle(cloneReq)
+    const isExcludedRoute = this.excludedRoutes.some(route => req.url.includes(route));
+
+    if (isExcludedRoute) {
+      return next.handle(req);
     } else {
-      return next.handle(req)
+      const token = localStorage.getItem('token');
+      if (token) {
+        const cloneReq = req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${token}`)
+        });
+        return next.handle(cloneReq);
+      } else {
+        return next.handle(req);
+      }
     }
   }
 }
@@ -26,4 +38,4 @@ export const AuthInterceptorProvider = [
     useClass: AuthInterceptor,
     multi: true
   }
-]
+];
